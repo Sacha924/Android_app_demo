51debec61921650145362a9c2bd6bc61aaa89281
Sacha924 <sacha.simon@edu.devinci.fr>
2023-12-18 16:13:55 +0100

    first page working, button sends us to second page, quizz page

---
+++ app/src/main/java/com/example/myapplication/QuizFragment.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QuizFragment extends Fragment {
+
+    public static QuizFragment newInstance() {
+        QuizFragment fragment = new QuizFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
+    }
+}
--- app/src/main/java/com/example/myapplication/WelcomeFragment.java
+++ app/src/main/java/com/example/myapplication/WelcomeFragment.java
@@ -1,14 +1,25 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+        import androidx.annotation.NonNull;
+        import androidx.annotation.Nullable;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentManager;
+        import androidx.fragment.app.FragmentTransaction;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.text.Editable;
+        import android.text.TextWatcher;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import com.example.myapplication.databinding.FragmentWelcomeBinding;
+        import android.util.Log;
 
 public class WelcomeFragment extends Fragment {
+    private FragmentWelcomeBinding binding;
+
     public static WelcomeFragment newInstance() {
         WelcomeFragment fragment = new WelcomeFragment();
         return fragment;
@@ -23,6 +34,47 @@ public class WelcomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(false);
+
+        binding.usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.playButton.setEnabled(!editable.toString().isEmpty());
+            }
+        });
+
+        binding.playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                QuizFragment quizFragment = QuizFragment.newInstance();
+
+                fragmentTransaction.add(R.id.fragment_container_view, quizFragment);
+
+                fragmentTransaction.addToBackStack(null);
+
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
