1e3ca3d536d9f800fbbe6500e9d25aa330fe4f1a
Sacha924 <sacha.simon@edu.devinci.fr>
2023-12-18 10:11:33 +0100

    update

---
+++ app/src/main/java/com/example/myapplication/WelcomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelcomeFragment extends Fragment {
+    public static WelcomeFragment newInstance() {
+        WelcomeFragment fragment = new WelcomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+}
